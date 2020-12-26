9ecb82551d83cf3bd629c6f4b7fd7679b0e44f06
Spladed <1310140596@qq.com>
2020-12-26 21:16:45 +0800

    完成5题

--- src/_5longestPalindrome.java
+++ src/_5longestPalindrome.java
@@ -1,29 +1,35 @@
 public class _5longestPalindrome {
 
+    /**
+     * 思路：对于s=ababd,若s是回文字符串，则必有s[0]=s[4] && s（1,3）是回文字符
+     * 若s（1,3）（即bab）是回文字符串，则必有s[1]=s[3] && s(2,2)是回文字符串
+     *
+     * @param s 字符串
+     * @return 最长回文子串
+     */
     public static String longestPalindrome(String s) {
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
         String ans = "";
-        for (int l = 0; l < n; ++l) {
-            for (int i = 0; i + l < n; ++i) {
-                int j = i + l;
-                if (l == 0) {
-                    dp[i][j] = true;
-                } else if (l == 1) {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                } else {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
-                }
-                if (dp[i][j] && l + 1 > ans.length()) {
-                    ans = s.substring(i, i + l + 1);
-                }
+        //dp[i][j]表示字符串从i到j的部分是否为回文字符串
+        boolean[][] dp = new boolean[n][n];
+        for (int len = 0; len < n; len++) {
+            for (int head = 0; head + len < n; head++) {
+                int tail = head + len;
+                if (len == 0)
+                    dp[head][tail] = true;
+                else if (len == 1)
+                    dp[head][tail] = s.charAt(head) == s.charAt(tail);
+                else
+                    dp[head][tail] = (s.charAt(head) == s.charAt(tail)) && dp[head + 1][tail - 1];
+                if (dp[head][tail] && len + 1 > ans.length())
+                    ans = s.substring(head, tail + 1);
             }
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        String s="ababd";
+        String s = "ababd";
         System.out.println(longestPalindrome(s));
     }
 
